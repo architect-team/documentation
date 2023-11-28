61a0df84e74330a75fd81d94d29d4bd47a93ff5d
David Thor <david.thor@arcussolutions.com>
2023-11-28 17:28:50 -0500

    Fixed a bunch of broken links

--- .bin/import-post.ts
+++ .bin/import-post.ts
@@ -11,71 +11,66 @@ const error = (message: string) => {
 };
 
 if (Deno.args.length <= 0) {
-  error('No RSS feed specified');
+  error('No URL specified');
 }
-
-const rssParser = new Rss();
-const feed = await rssParser.parseURL(Deno.args[0]);
 
 const turndownService = new Turndown();
 
-for (const item of feed.items) {
-  const slug = item.link
-    .split('/')
-    .filter((i) => i !== '')
-    .pop();
-  const date = new Date(item.pubDate);
+const slug = Deno.args[0]
+  .split('/')
+  .filter((i) => i !== '')
+  .pop();
+const date = new Date(item.pubDate);
 
-  const month = date.getMonth() + 1;
-  let monthStr = '';
-  if (month < 10) {
-    monthStr = `0${month}`;
-  } else {
-    monthStr = month.toString();
-  }
+const month = date.getMonth() + 1;
+let monthStr = '';
+if (month < 10) {
+  monthStr = `0${month}`;
+} else {
+  monthStr = month.toString();
+}
 
-  const day = date.getDate();
-  let dayStr = '';
-  if (day < 10) {
-    dayStr = `0${day}`;
-  } else {
-    dayStr = day.toString();
-  }
+const day = date.getDate();
+let dayStr = '';
+if (day < 10) {
+  dayStr = `0${day}`;
+} else {
+  dayStr = day.toString();
+}
 
-  const folderSlug = `${date.getFullYear()}-${monthStr}-${dayStr}-${slug}`;
-  const fileSlug = `${folderSlug}/index.mdx`;
+const folderSlug = `${date.getFullYear()}-${monthStr}-${dayStr}-${slug}`;
+const fileSlug = `${folderSlug}/index.mdx`;
 
-  // Retrieve markdown contents
-  const data = await fetch(item.link);
-  const html = await data.text();
-  const parsedHtml = HtmlParser.parse(html);
-  const main = parsedHtml.querySelector('main');
-  if (!main) {
-    console.error('%cNo <main> element found in ' + folderSlug, 'color: red');
-    continue;
-  }
-  const mainStr = main.toString();
-  const markdown = turndownService.turndown(mainStr, {
-    codeBlockStyle: 'fenced',
-  });
+// Retrieve markdown contents
+const data = await fetch(item.link);
+const html = await data.text();
+const parsedHtml = HtmlParser.parse(html);
+const main = parsedHtml.querySelector('main');
+if (!main) {
+  console.error('%cNo <main> element found in ' + folderSlug, 'color: red');
+  continue;
+}
+const mainStr = main.toString();
+const markdown = turndownService.turndown(mainStr, {
+  codeBlockStyle: 'fenced',
+});
 
-  try {
-    await Deno.readTextFile(path.join(mainModuleDir, `../blog/${fileSlug}`));
-    console.log('Skipped: ' + folderSlug);
-  } catch (err) {
-    if (err.name === 'NotFound') {
-      await Deno.mkdir(path.join(mainModuleDir, `../blog/${folderSlug}`));
-      await Deno.writeTextFile(
-        path.join(mainModuleDir, `../blog/${fileSlug}`),
-        `---
+try {
+  await Deno.readTextFile(path.join(mainModuleDir, `../blog/${fileSlug}`));
+  console.log('Skipped: ' + folderSlug);
+} catch (err) {
+  if (err.name === 'NotFound') {
+    await Deno.mkdir(path.join(mainModuleDir, `../blog/${folderSlug}`));
+    await Deno.writeTextFile(
+      path.join(mainModuleDir, `../blog/${fileSlug}`),
+      `---
 title: ${item.title}
 ---
 ${markdown}`
-      );
+    );
 
-      console.log(`%cCreated: ${folderSlug}`, 'color: green');
-    } else {
-      throw err;
-    }
+    console.log(`%cCreated: ${folderSlug}`, 'color: green');
+  } else {
+    throw err;
   }
 }
